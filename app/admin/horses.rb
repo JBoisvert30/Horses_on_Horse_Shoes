00b172e6ab4b5a709af82c66b409d14ba795e622
Jon <jboisvert30@rrc.ca>
2024-03-19 11:41:30 -0500

    Added and displayed horse images on admin and production view

--- app/admin/horses.rb
+++ app/admin/horses.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Horse do
- permit_params :name, :age, :top_speed, :breed_id, :number_of_legs
+ permit_params :name, :age, :top_speed, :breed_id, :number_of_legs, :image
+
+ form do |f|
+  f.semantic_errors # shows errors on :base
+  f.inputs          # builds an input field for every attribute
+  f.inputs do
+    f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+  end
+  f.actions         # adds the 'Submit' and 'Cancel' buttons
+end
 
 end
